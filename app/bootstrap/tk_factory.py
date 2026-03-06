43fa18a30a5b32243a908611874a62fda552afdc
BenjuMoy <bemoyano@uade.edu.ar>
2026-03-06 17:01:02 -0300

    tk_factory refactor

--- app/bootstrap/tk_factory.py
+++ app/bootstrap/tk_factory.py
@@ -3,25 +3,30 @@ import ttkbootstrap as ttk
 from app.utils.constantes import ICON_PATH
 
 
+def _center_window(root, width, height):
+    screen_w = root.winfo_screenwidth()
+    screen_h = root.winfo_screenheight()
+
+    x = (screen_w // 2) - (width // 2)
+    y = (screen_h // 2) - (height // 2)
+
+    root.geometry(f"{width}x{height}+{x}+{y}")
+
+
 class TkAppFactory:
     @staticmethod
     def create_root(theme: str, title: str) -> ttk.Window:
         root = ttk.Window(themename=theme)
         root.title(title)
 
-        # Better geometry: 1280x800 centered
         width = 1366
         height = 768
-        screen_w = root.winfo_screenwidth()
-        screen_h = root.winfo_screenheight()
 
-        x = (screen_w // 2) - (width // 2)
-        y = (screen_h // 2) - (height // 2)
+        _center_window(root, width, height)
 
-        root.geometry(f"{width}x{height}+{x}+{y}")
         root.minsize(1280, 800)
 
-        icon = ttk.PhotoImage(file=ICON_PATH)
-        root.iconphoto(True, icon)
+        root._icon = ttk.PhotoImage(file=ICON_PATH)
+        root.iconphoto(True, root._icon)
 
         return root
