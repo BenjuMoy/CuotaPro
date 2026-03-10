3c2d2ba38e1ea9f0883a3ae4b56f0038729121ca
BenjuMoy <bemoyano@uade.edu.ar>
2026-03-10 17:03:53 -0300

    improved main

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from app.application import Application
 from app.utils.logger import setup_logging
 
 
-def main() -> None:
+def main() -> int:
     """Entry point for the application."""
     setup_logging()
+
     app = Application()
     app.run()
+
+    return 0
 
 
 if __name__ == "__main__":
