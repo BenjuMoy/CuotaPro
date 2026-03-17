51ce760eba56bbb63732a0c3a528d08c83426ff8
BenjuMoy <bemoyano@uade.edu.ar>
2026-03-17 01:13:33 -0300

    version update

--- app/utils/constantes.py
+++ app/utils/constantes.py
@@ -3,7 +3,7 @@
 # --------------------------------------------------------------------------- #
 from pathlib import Path
 
-APP_VERSION = "1.3.1"
+APP_VERSION = "1.3.2"
 
 LOGS_DIR = Path("logs/")
 LOGS_PATH = Path("logs/app.log")
