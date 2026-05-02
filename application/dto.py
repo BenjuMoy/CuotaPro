4bb4c749fd0f6d71f24a346b26998f8cd8efa199
BenjuMoy <bemoyano@uade.edu.ar>
2026-05-01 21:04:58 -0300

    Move validation to domain

--- application/dto.py
+++ application/dto.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from typing import Literal
 
 from pydantic import BaseModel, Field
 
@@ -18,62 +17,12 @@ class CreateStudentDTO(BaseModel):
     phone2: str = ""
     phone3: str = ""
 
-    teacher: Literal["Asuncion", "Daniela", "Florencia", "Kiana", "Romina", "Silvia"]
+    teacher: str
 
     school: str = ""
-    school_year: Literal[
-        "",
-        "Kindergarden",
-        "1 EP",
-        "2 EP",
-        "3 EP",
-        "4 EP",
-        "5 EP",
-        "6 EP",
-        "1 ES",
-        "2 ES",
-        "3 ES",
-        "4 ES",
-        "5 ES",
-        "6 ES",
-    ] = ""
-    book: Literal[
-        "",
-        "Power Up Start P. 1",
-        "Power Up Start P. 2",
-        "Learn With Us 1",
-        "Power Up 1",
-        "Power Up 2",
-        "Own It 1",
-        "Gateway A1",
-        "Gateway A2",
-        "Gateway B1",
-        "Gateway B2",
-        "Gold. Exp. FCE",
-        "Gold. Exp. CAE",
-        "Insight Elem.",
-        "Insight Pre. Int.",
-        "Insight Int.",
-    ] = ""
-    course: Literal[
-        "",
-        "Kids 1",
-        "Kids 2",
-        "Kids 3",
-        "Junior 1",
-        "Junior 2",
-        "Junior 3",
-        "Senior 1",
-        "Senior 2",
-        "Senior 3",
-        "Senior 4",
-        "Senior 5",
-        "Senior 6",
-        "Adults 1",
-        "Adults 2",
-        "Adults 3",
-        "Adults 4",
-    ] = ""
+    school_year: str = ""
+    book: str = ""
+    course: str = ""
 
     monthly_fee: int = Field(gt=0)
 
--- domain/student/model.py
+++ domain/student/model.py
@@ -1,9 +1,64 @@
-from domain.shared.exceptions import BusinessRuleError
+from domain.shared.exceptions import AppValidationError, BusinessRuleError
 from domain.student.values import MonthlyFee, PhoneNumber, StudentName
 
 # last_name, first_name, telefons, school, year, teacher, book, course, monthly_fee, balance
 
 # TODO Make this more passive, this is not the aggregate
+
+VALID_TEACHERS = {"Asuncion", "Daniela", "Florencia", "Kiana", "Romina", "Silvia"}
+VALID_SCHOOL_YEARS = {
+    "",
+    "Kindergarden",
+    "1 EP",
+    "2 EP",
+    "3 EP",
+    "4 EP",
+    "5 EP",
+    "6 EP",
+    "1 ES",
+    "2 ES",
+    "3 ES",
+    "4 ES",
+    "5 ES",
+    "6 ES",
+}
+VALID_BOOKS = {
+    "",
+    "Power Up Start P. 1",
+    "Power Up Start P. 2",
+    "Learn With Us 1",
+    "Power Up 1",
+    "Power Up 2",
+    "Own It 1",
+    "Gateway A1",
+    "Gateway A2",
+    "Gateway B1",
+    "Gateway B2",
+    "Gold. Exp. FCE",
+    "Gold. Exp. CAE",
+    "Insight Elem.",
+    "Insight Pre. Int.",
+    "Insight Int.",
+}
+VALID_COURSES = {
+    "",
+    "Kids 1",
+    "Kids 2",
+    "Kids 3",
+    "Junior 1",
+    "Junior 2",
+    "Junior 3",
+    "Senior 1",
+    "Senior 2",
+    "Senior 3",
+    "Senior 4",
+    "Senior 5",
+    "Senior 6",
+    "Adults 1",
+    "Adults 2",
+    "Adults 3",
+    "Adults 4",
+}
 
 
 class Student:
@@ -53,6 +108,8 @@ class Student:
         self.monthly_fee = monthly_fee
         self.active = active
 
+        self._validate()
+
     def change_monthly_fee(self, new_fee: MonthlyFee):
         if self.monthly_fee.amount == new_fee.amount:
             raise BusinessRuleError("Las cuotas no pueden ser iguales")
@@ -62,6 +119,15 @@ class Student:
 
         self.monthly_fee = new_fee
 
-    def ensure_active(self):
-        if not self.active:
-            raise BusinessRuleError("Estudiante inactivo")
+    def _validate(self):
+        if self.teacher not in VALID_TEACHERS:
+            raise AppValidationError("Profesor invalido")
+
+        if self.school_year not in VALID_SCHOOL_YEARS:
+            raise AppValidationError("Año escolar invalido")
+
+        if self.book not in VALID_BOOKS:
+            raise AppValidationError("Libro invalido")
+
+        if self.course not in VALID_SCHOOL_YEARS:
+            raise AppValidationError("curso invalido")
