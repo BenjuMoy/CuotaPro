f422fffc7f87c18cc17a570a85749b7f29caa20b
BenjuMoy <bemoyano@uade.edu.ar>
2026-05-04 17:52:53 -0300

    refactor dto

--- application/dto.py
+++ application/dto.py
@@ -9,10 +9,9 @@ from domain.shared.shared import MovementType
 # --------------------------------------------------------------------------- #
 
 
-class BaseStudentDTO(BaseModel): ...  # TODO
+class BaseStudentDTO(BaseModel):
+    """Base model containing common student fields."""
 
-
-class CreateStudentDTO(BaseModel):
     first_name: str = Field(min_length=1, max_length=50)
     last_name: str = Field(min_length=1, max_length=50)
 
@@ -22,32 +21,28 @@ class CreateStudentDTO(BaseModel):
 
     teacher: str
 
+    monthly_fee: int = Field(gt=0)
+
+
+class CreateStudentDTO(BaseStudentDTO):
+    """DTO for creating a new student with optional fields."""
+
     school: str = ""
     school_year: str = ""
     book: str = ""
     course: str = ""
 
-    monthly_fee: int = Field(gt=0)
 
+class StudentDTO(BaseStudentDTO):
+    """DTO for student representation with all required fields."""
 
-class StudentDTO(BaseModel):
     id: int
     active: bool
 
-    first_name: str
-    last_name: str
-
-    phone1: str
-    phone2: str = ""
-    phone3: str = ""
-
-    teacher: str
     school: str
     school_year: str
     book: str
     course: str
-
-    monthly_fee: int
 
 
 # --------------------------------------------------------------------------- #
